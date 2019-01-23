f14e7452c9cc42713a52296b6e54f0f94975338c
Alexander Tonkin <alextonkin@gmail.com>
2019-01-23 15:16:53 +0200

    Update userLogin.php

--- userLogin.php
+++ userLogin.php
@@ -2,9 +2,13 @@
 <?php
 $params = $_GET;
 print_r($params);
-$original_url = urldecode($params['original-url']);
-$original_url .= '&username=bob&confirmation=bob';
-$original_url = urlencode($original_url);
+$original_url = $params['original-url'];
+$original_url_arr = parse_url($original_url);
+print_r($original_url_arr);
+$original_url_params = urldecode($original_url_arr['query']);
+$original_url_params .= '&username=bob&confirmation=bob';
+$original_url_params = urlencode($original_url_params);
+$original_url = sprintf("%s://%s%s?%s", $original_url_arr['scheme'], $original_url_arr['host'], $original_url_arr['path'], $original_url_params);
 ?>
 </pre>
 <p><?= sprintf("Original URL: %s", $original_url); ?></p>
