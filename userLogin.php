35077d8b32b13182ed340376c24770ee3291026e
Alexander Tonkin <alextonkin@gmail.com>
2019-01-23 14:43:47 +0200

    Update userLogin.php

--- userLogin.php
+++ userLogin.php
@@ -1,5 +1,8 @@
 <pre>
-<?php 
-print_r($_GET);
+<?php
+$params = $_GET;
+print_r($params);
+$original_url = urldecode($params['original-url']);
+sprintf("Original URL: %s", $original_url);
 ?>
 </pre>
