d1c70c6bd9eadd70a37231bb72bed2cac192f18a
Alexander Tonkin <alextonkin@gmail.com>
2019-01-23 14:53:51 +0200

    Update userLogin.php

--- userLogin.php
+++ userLogin.php
@@ -3,6 +3,8 @@
 $params = $_GET;
 print_r($params);
 $original_url = urldecode($params['original-url']);
+$original_url .= '&username=bob&confirmation=bob';
 ?>
 </pre>
 <p><?= sprintf("Original URL: %s", $original_url); ?></p>
+<p><a href="<?= $original_url ?>">Confirm</a></p>
